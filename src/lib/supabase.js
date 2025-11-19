1c5b30397659a425a9559cdb8836d9d72d0f4ab7
ilensf <lenssoftware@gmail.com>
2025-11-20 00:48:39 +0100

    lib

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,10 +1,12 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error('Faltan as credenciais públicas de Supabase nas variables de entorno');
+  throw new Error(
+    "Faltan as credenciais públicas de Supabase nas variables de entorno"
+  );
 }
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);

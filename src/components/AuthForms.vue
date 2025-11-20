5dfdf3899b813435db2ee4d247693302560e08df
ilensf <lenssoftware@gmail.com>
2025-11-20 01:26:46 +0100

    lib

--- src/components/AuthForms.vue
+++ src/components/AuthForms.vue
@@ -1,5 +1,5 @@
 <script>
-import { supabase } from '/src/lib/supabase.js';
+import { supabase } from '../lib/supabase';
 export default {
   data() {
     return {
--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,6 +1,6 @@
 
 <script>
-import { supabase } from '/src/lib/supabase.js';
+import { supabase } from '../lib/supabase';
 export default {
   components: {
     LoginTransition,

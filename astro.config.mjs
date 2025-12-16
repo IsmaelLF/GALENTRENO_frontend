e605b9cd5b80f27114957c44f747a0b2415a1009
ilensf <lenssoftware@gmail.com>
2025-12-16 19:21:14 +0100

    Presentacion

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import vue from "@astrojs/vue";
-
+import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel";
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [vue()],
+  integrations: [vue(), tailwind()],
   adapter: vercel(),
 });
